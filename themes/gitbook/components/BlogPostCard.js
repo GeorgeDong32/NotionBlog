57c4a916bd1a986e8c1df97cf8c037610afe21e3
George·Dong <georgedong32@foxmail.com>
2024-01-31 22:04:33 +0800

    Revert "Gitbook导航微调"
    This reverts commit 3a9da4ed6aff6979f7e955e418480e7ee334e5e3.

--- themes/gitbook/components/BlogPostCard.js
+++ themes/gitbook/components/BlogPostCard.js
@@ -6,12 +6,13 @@ const BlogPostCard = ({ post, className }) => {
   const router = useRouter()
   const currentSelected = router.asPath.split('?')[0] === '/' + post.slug
   return (
-        <Link href={`${siteConfig('SUB_PATH', '')}/${post.slug}`} passHref> <div key={post.id} className={`${className} py-1.5 cursor-pointer px-1.5 hover:bg-gray-50 rounded-md dark:hover:bg-gray-600  ${currentSelected ? 'bg-green-50 text-green-500' : ''}`}>
+        <div key={post.id} className={`${className} py-1 cursor-pointer px-2 hover:bg-gray-50 rounded-md dark:hover:bg-gray-600  ${currentSelected ? 'bg-green-50 text-green-500' : ''}`}>
             <div className="flex flex-col w-full select-none">
-                {post.title}
+                <Link href={`${siteConfig('SUB_PATH', '')}/${post.slug}`} passHref>
+                    {post.title}
+                </Link>
             </div>
         </div>
-        </Link>
   )
 }
 
